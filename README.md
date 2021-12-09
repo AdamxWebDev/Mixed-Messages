1857b1632dd6dfee1c5b8e7532b91b0699a2b77e
Adam WebDev <Adam_WebDev@outlook.com>
2021-12-09 10:55:57 -0700

    A randomized inspirational quote generator.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Mixed-Messages
-Randomized Message Generator
+#Randomized Message Generator#
+
+For this project, you will build a message generator program. Every time a user runs a program, they should get a new, randomized output. You’re welcome to take the project in a couple of different forms, like an astrology generator, inspirational message, or nonsensical jokes. To make your program truly random, the message that it outputs should be made up of at least three different pieces of data. Take what you know of JavaScript syntax so far to build the program and customize it to your liking.
+
+**Project Objectives:**
+-Build a message generator program using JavaScript
+-Use Git version control
+-Use command line
+-Develop locally on your computer
+**Prerequisites:**
+-JavaScript
+-Git and GitHub
+-Command line
+
+---
+
+In this project I decided to use insperational quotes. I created 50 variables with the strings and then put them into an array. Creating a random number using that Math object I made a function that when an array is entered as an argument calls a random index number and logs the stored string.
+
+---
+
+[Source: Codecademy]: https://www.codecademy.com/paths/full-stack-engineer-career-path/tracks/fscp-javascript-syntax-portfolio-project/modules/fscp-mixed-messages/kanban_projects/mixed-messages
+
+---
+
---
+++ generator.js
@@ -0,0 +1,113 @@
+const message1 = `“When you have a dream, you’ve got to grab it and never let go.”
+— Carol Burnett`;
+const message2 = `“Nothing is impossible. The word itself says ‘I’m possible!'”
+— Audrey Hepburn`;
+const message3 = `“There is nothing impossible to they who will try.”
+— Alexander the Great`;
+const message4 = `“The bad news is time flies. The good news is you’re the pilot.”
+— Michael Altshuler`;
+const message5 = `“Life has got all those twists and turns. You’ve got to hold on tight and off you go.”
+— Nicole Kidman`;
+const message6 = `“Keep your face always toward the sunshine, and shadows will fall behind you.”
+— Walt Whitman`;
+const message7 = `“Be courageous. Challenge orthodoxy. Stand up for what you believe in. When you are in your rocking chair talking to your grandchildren many years from now, be sure you have a good story to tell.”
+— Amal Clooney`;
+const message8 = `“You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding, or you find your identity independent of it. You draw your own box.”
+— Duchess Meghan`;
+const message9 = `“I just want you to know that if you are out there and you are being really hard on yourself right now for something that has happened … it’s normal. That is what is going to happen to you in life. No one gets through unscathed. We are all going to have a few scratches on us. Please be kind to yourselves and stand up for yourself, please.”
+— Taylor Swift`;
+const message10 = `“Success is not final, failure is not fatal: it is the courage to continue that counts.”
+– Winston Churchill`;
+const message11 = `“You define your own life. Don’t let other people write your script.”
+— Oprah Winfrey`;
+const message12 = `“You are never too old to set another goal or to dream a new dream.”
+— Malala Yousafzai`;
+const message13 = `“At the end of the day, whether or not those people are comfortable with how you’re living your life doesn’t matter. What matters is whether you’re comfortable with it.”
+— Dr. Phil`;
+const message14 = `“People tell you the world looks a certain way. Parents tell you how to think. Schools tell you how to think. TV. Religion. And then at a certain point, if you’re lucky, you realize you can make up your own mind. Nobody sets the rules but you. You can design your own life.”
+— Carrie Ann Moss`;
+const message15 = ` “For me, becoming isn’t about arriving somewhere or achieving a certain aim. I see it instead as forward motion, a means of evolving, a way to reach continuously toward a better self. The journey doesn’t end.”
+— Michelle Obama`;
+const message16 = `“Spread love everywhere you go.”
+— Mother Teresa`;
+const message17 = `“Do not allow people to dim your shine because they are blinded. Tell them to put some sunglasses on.”
+— Lady Gaga`;
+const message18 = `“If you make your internal life a priority, then everything else you need on the outside will be given to you and it will be extremely clear what the next step is.”
+— Gabrielle Bernstein`;
+const message19 = `“You don’t always need a plan. Sometimes you just need to breathe, trust, let go and see what happens.”
+— Mandy Hale`;
+const message20 = `“You can be everything. You can be the infinite amount of things that people are.”
+— Kesha`;
+const message21 = `“What lies behind you and what lies in front of you, pales in comparison to what lies inside of you.”
+— Ralph Waldo Emerson`;
+const message22 = `“I want to be in the arena. I want to be brave with my life. And when we make the choice to dare greatly, we sign up to get our asses kicked. We can choose courage or we can choose comfort, but we can’t have both. Not at the same time.”
+— Brene Brown`;
+const message23 = `“I’m going to be gone one day, and I have to accept that tomorrow isn’t promised. Am I OK with how I’m living today? It’s the only thing I can help. If I didn’t have another one, what have I done with all my todays? Am I doing a good job?”
+— Hayley Williams`;
+const message24 = `“Belief creates the actual fact.”
+— William James`;
+const message25 = `“No matter what people tell you, words and ideas can change the world.”
+— Robin Williams as John Keating in Dead Poets Society`;
+const message26 = `“I’m not going to continue knocking that old door that doesn’t open for me. I’m going to create my own door and walk through that.”
+— Ava DuVernay`;
+const message27 = `“It is during our darkest moments that we must focus to see the light.”
+— Aristotle`;
+const message28 = `“Not having the best situation, but seeing the best in your situation is the key to happiness.”
+— Marie Forleo`;
+const message29 = `“Believe you can and you’re halfway there.”
+– Theodore Roosevelt`;
+const message30 = `“Weaknesses are just strengths in the wrong environment.”
+— Marianne Cantwell`;
+const message31 = `“Just don’t give up trying to do what you really want to do. Where there is love and inspiration, I don’t think you can go wrong.”
+— Ella Fitzgerald`;
+const message32 = `“Silence is the last thing the world will ever hear from me.”
+— Marlee Matlin`;
+const message33 = `“In a gentle way, you can shake the world.”
+— Mahatma Gandhi`;
+const message34 = `“Learning how to be still, to really be still and let life happen—that stillness becomes a radiance.”
+— Morgan Freeman`;
+const message35 = `“Everyone has inside of him a piece of good news. The good news is that you don’t know how great you can be! How much you can love! What you can accomplish! And what your potential is!”
+— Anne Frank`;
+const message36 = `“All you need is the plan, the road map, and the courage to press on to your destination.”
+— Earl Nightingale`;
+const message37 = `“I care about decency and humanity and kindness. Kindness today is an act of rebellion.”
+— Pink`;
+const message38 = `“If you have good thoughts they will shine out of your face like sunbeams and you will always look lovely.”
+— Roald Dahl`;
+const message39 = `“Try to be a rainbow in someone’s cloud.”
+— Maya Angelou`;
+const message40 = `“We must let go of the life we have planned, so as to accept the one that is waiting for us.”
+— Joseph Campbell`;
+const message41 = `“Find out who you are and be that person. That’s what your soul was put on this earth to be. Find that truth, live that truth, and everything else will come.”
+— Ellen DeGeneres`;
+const message42 = `“Real change, enduring change, happens one step at a time.”
+— Ruth Bader Ginsburg`;
+const message43 = `“Wake up determined, go to bed satisfied.”
+— Dwayne “The Rock” Johnson`;
+const message44 = `“Nobody built like you, you design yourself.”
+— Jay-Z`;
+const message45 = `“You gain strength, courage, and confidence by every experience in which you really stop to look fear in the face. You are able to say to yourself, ‘I lived through this horror. I can take the next thing that comes along.’ You must do the thing you think you cannot do.”
+— Eleanor Roosevelt`;
+const message46 = `“I tell myself, ‘You’ve been through so much, you’ve endured so much, time will allow me to heal, and soon this will be just another memory that made me the strong woman, athlete, and mother I am today.”‘
+— Serena Williams`;
+const message47 = `“Live your beliefs and you can turn the world around.”
+— Henry David Thoreau`;
+const message48 = `“Our lives are stories in which we write, direct and star in the leading role. Some chapters are happy while others bring lessons to learn, but we always have the power to be the heroes of our own adventures.”
+— Joelle Speranza`;
+const message49 = `“Life is like riding a bicycle. To keep your balance, you must keep moving.”
+– Albert Einstein`;
+const message50 = `“Don’t try to lessen yourself for the world; let the world catch up to you.”
+— Beyoncé`;
+
+const messageArray = [message1,message2,message3,message4,message5,message6,message7,message8,message9,message10,
+    message11,message12,message13,message14,message15,message16,message17,message18,message19,message20,message21,
+    message22,message23,message24,message25,message26,message27,message28,message29,message30,message31,message32,
+    message33,message34,message35,message36,message37,message38,message39,message40,message41,message42,message43,
+    message44,message45,message46,message47,message48,message49,message50,]
+
+const randomQuote = (arr) => {
+    let randIndex = Math.floor(Math.random() * 50)
+    return console.log(`${arr[randIndex]}`)
+};
+
+randomQuote(messageArray)
